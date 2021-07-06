1b43b0943ff66a1e04949b85bb232041f874872d
Rattanachai Sriwilai <duk151@hotmail.com>
2021-07-07 01:07:38 +0200

    added new shoppingList & recipeBook components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListEditComponent,
+
   ],
   imports: [
     BrowserModule
